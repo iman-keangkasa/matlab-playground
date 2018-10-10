c5afeac17e0524a2cfb37590c7b6880f7efdc3f2
Iman <hafiz.ghazman@gmail.com>
2018-10-10 18:32:55 +0800

    Add rotational matrix

---
+++ x_rot.m
@@ -0,0 +1,3 @@
+function s = x_rot(theta)
+s = [1, 0, 0; 0, cos(theta), -sin(theta); 0, sin(theta), cos(theta)];
+end
---
+++ y_rot.m
@@ -0,0 +1,3 @@
+function s=y_rot(theta)
+s=[cos(theta), 0, sin(theta); 0, 1, 0; -sin(theta), 0, cos(theta)];
+end
